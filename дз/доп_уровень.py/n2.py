c59ed2835bf19e760bf52aff1b6e24ff5d3ea4af
Goida42 <shamo072805337@gmail.com>
2025-11-12 21:55:15 +0000

    123

--- дз/доп_уровень.py/n2.py
+++ дз/доп_уровень.py/n2.py
@@ -1,1 +1,16 @@
-print("kkk")
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a + b > c and a + c > b and b + c > a:
+    print("треугольник существует")
+
+    if a == b == c:
+        print("треугольник равносторонний")
+    elif a == b or a == c or b == c:
+        print("треугольник равнобедренный")
+    else:
+        print("треугольник разносторонний")
+
+else:
+    print("треугольник не существует")
