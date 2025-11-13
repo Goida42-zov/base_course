37d14cd8949df445c26e6f38ca64b3be81930a4e
Goida42 <shamo072805337@gmail.com>
2025-11-13 15:32:43 +0000

    kk

--- дз/дз6.py
+++ дз/дз6.py
@@ -1,10 +1,4 @@
-for i, a, u , s, d, f, g, h, j in zip(range(1, 10), 
-                                      range(2, 19, 2), 
-                                      range(3, 28, 3), 
-                                      range(4, 37, 4), 
-                                      range(5, 46, 5), 
-                                      range(6, 55 , 6), 
-                                      range(7, 64, 7), 
-                                      range(8, 73, 8), 
-                                      range(9, 82, 9)):
-    print(i, a, u , s, d, f, g, h, j)
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i * j, end=' ')
+    print()
